d966e3bd3f8f66d26d07f510775230ba4b341e36
yowanskie <juanxxlizares@gmail.com>
2025-03-31 11:23:55 +0800

    Revert "Created a Usermodel Using Bcrypt"

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
+         validates :email,
+         presence: true,
+         uniqueness: true,
+         format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  has_secure_password
+       before_validation :normalize_email
 
-  validates :email,
-    presence: true,
-    uniqueness: true,
-    format: { with: URI::MailTo::EMAIL_REGEXP }
-
-
-  validates :password,
-    length: { minimum: 6 },
-    if: -> { new_record? || !password.nil? }
-
-
-  before_validation :normalize_email
-
-  private
-  def normalize_email
-    self.email = email.to_s.strip.downcase
-    self.email = nil if email.blank?
-  end
+       private
+       def normalize_email
+         self.email = email.to_s.strip.downcase
+         self.email = nil if email.blank?
+       end
 end
