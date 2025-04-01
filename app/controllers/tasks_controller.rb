8e6d23d24c0f16bed4e4748d9cb8ba2291543c6a
tyryll <dalepatos14@gmail.com>
2025-04-01 09:36:53 +0800

    Fix lint using rubocop

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,16 @@
 class TasksController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_task, only: [:show, :update, :destroy, :complete]
+    before_action :set_task, only: [ :show, :update, :destroy, :complete ]
 
     def index
       @tasks = current_user.tasks
       render json: @tasks
     end
-  
+
     def show
       render json: @task
     end
-  
+
     def create
       @task = current_user.tasks.build(task_params)
       if @task.save
@@ -19,7 +19,7 @@ class TasksController < ApplicationController
         render json: @task.errors, status: :unprocessable_entity
       end
     end
-  
+
     def update
       if @task.update(task_params)
         render json: @task
@@ -27,23 +27,23 @@ class TasksController < ApplicationController
         render json: @task.errors, status: :unprocessable_entity
       end
     end
-  
+
     def complete
       @task.update(completed: true)
       render json: @task
     end
-  
+
     def destroy
       @task.destroy
       head :no_content
     end
-  
+
     private
-  
+
     def set_task
       @task = current_user.tasks.find(params[:id])
     end
-  
+
     def task_params
       params.require(:task).permit(:title, :description, :due_date, :completed)
     end
