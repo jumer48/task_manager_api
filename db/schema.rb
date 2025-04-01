e7e35d30850133f8d08ca1a2e6f1fa87decccbb2
tyryll <dalepatos14@gmail.com>
2025-04-01 17:06:10 +0800

    Add relationship to task and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_01_071838) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_01_081125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_01_071838) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +47,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_01_071838) do
     t.index ["jti"], name: "index_users_on_jti"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "tasks", "users"
 end
