0f9e7efc0248de08f5902c4f40f925e435a93e15
glophelyn <glophelynjocson00@gmail.com>
2025-04-01 21:23:46 +0800

    Implement POST/task

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   test "should get index" do
-    User.create(email: "test@example.com", password: "securepassword")
+    user = User.create(email: "test@example.com", password: "securepassword")
+    sign_in user
     get users_url
     assert_response :success
   end
