5f0d8ce29a3dc79a6aec5d20c32b62e8bb4e946b
Glophelyn <glophelynjocson00@gmail.com>
2025-04-02 09:23:20 +0800

    Validate title and due_date (#47)
    * Validate title and due_date
    
    * fix whitespace error

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :user
+
+    # validate presence of title and due_date on the tasks
+    validates :title, presence: true
+    validates :due_date, presence: true
 end
